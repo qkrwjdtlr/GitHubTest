d0c05525d407f4b641deaa4b653ad111574fa3e4
qkrwjdtlr <qkrwjdtlrz@naver.com>
2025-05-20 10:16:32 +0900

    develop2

--- Assets/develop/develop2.cs
+++ Assets/develop/develop2.cs
@@ -7,7 +7,7 @@ public class develop2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //develop2
     }
 
     // Update is called once per frame
